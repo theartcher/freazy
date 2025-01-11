27c2aff0a10a3872813a54cb7833039a2bf7abcc
theartcher <jorisplayz@gmail.com>
2025-01-11 17:46:15 +0100

    Add basic notification background task

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:freazy/constants/routes.dart';
 import 'package:freazy/stores/item-store.dart';
+import 'package:freazy/utils/notification_helper.dart';
 import 'package:provider/provider.dart';
+import 'package:workmanager/workmanager.dart';
+
+@pragma(
+    'vm:entry-point') // Mandatory if the App is obfuscated or using Flutter 3.1+
+void callbackDispatcher() {
+  Workmanager().executeTask((task, inputData) {
+    return NotificationHelper().sendNotifications();
+  });
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  Workmanager().initialize(
+    callbackDispatcher,
+    isInDebugMode: true,
+  );
+
+  Workmanager().registerPeriodicTask(
+    "check-for-notifications",
+    "notificationCheck",
+    frequency: const Duration(days: 1),
+    backoffPolicy: BackoffPolicy.exponential,
+    backoffPolicyDelay: const Duration(hours: 1),
+  );
+
   runApp(ChangeNotifierProvider(
     create: (context) => FrozenItemStore(),
     child: const MainApp(),
