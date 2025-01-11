d38e969039bec0813c9b3129bdcff4b5ce20d48f
theartcher <jorisplayz@gmail.com>
2025-01-11 17:46:27 +0100

    Add preferences manager for durations

---
+++ lib/utils/preferences_manager.dart
@@ -0,0 +1,25 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class PreferencesManager {
+  Future<void> saveDurations(List<Duration> durations) async {
+    final preferences = await SharedPreferences.getInstance();
+
+    final durationList = durations.map((d) => d.inMilliseconds).toList();
+
+    await preferences.setStringList(
+        'durations', durationList.map((d) => d.toString()).toList());
+  }
+
+  Future<List<Duration>> loadDurations() async {
+    final preferences = await SharedPreferences.getInstance();
+    final durationList = preferences.getStringList('durations');
+
+    if (durationList == null) {
+      return [];
+    }
+
+    return durationList
+        .map((d) => Duration(milliseconds: int.parse(d)))
+        .toList();
+  }
+}
