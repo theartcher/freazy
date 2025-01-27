61703e62c426d45891c1f4c93098a6666a072c4f
theartcher <jorisplayz@gmail.com>
2025-01-27 19:26:27 +0100

    W.I.P Settings page localization

--- lib/pages/settings/settings.dart
+++ lib/pages/settings/settings.dart
@@ -3,6 +3,7 @@ import 'package:freazy/widgets/settings/personalization/personalization_setting.
 import 'package:freazy/widgets/settings/reminders/reminders-settings.dart';
 import 'package:freazy/widgets/settings/reset_app.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
@@ -15,13 +16,14 @@ class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final localization = AppLocalizations.of(context)!;
 
     return Scaffold(
       appBar: AppBar(
         backgroundColor: theme.colorScheme.primary,
         foregroundColor: theme.colorScheme.onPrimary,
         scrolledUnderElevation: 0,
-        title: const Text('Instellingen'),
+        title: Text(localization.settingsPage_header_title),
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
           onPressed: () => context.pop(),
@@ -32,11 +34,12 @@ class _SettingsPageState extends State<SettingsPage> {
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
             child: Text(
-              'Meldingen',
+              localization.settingsPage_remindersSection_header,
               style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: theme.colorScheme.primary),
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: theme.colorScheme.primary,
+              ),
             ),
           ),
           const ReminderSettings(),
@@ -44,11 +47,12 @@ class _SettingsPageState extends State<SettingsPage> {
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
             child: Text(
-              'Personalisatie',
+              localization.settingsPage_personalizationSection_header,
               style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: theme.colorScheme.primary),
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: theme.colorScheme.primary,
+              ),
             ),
           ),
           const PersonalizationSetting(),
--- lib/widgets/settings/personalization/theme_selector.dart
+++ lib/widgets/settings/personalization/theme_selector.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:freazy/widgets/settings/pressable_setting.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class ThemeSelector extends StatefulWidget {
   final ThemeMode selectedTheme;
@@ -19,10 +20,12 @@ class _ThemeSelectorState extends State<ThemeSelector> {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final localization = AppLocalizations.of(context)!;
 
     return PressableSettingTile(
-      title: "Thema selecteren",
-      description: "Kies het thema van de app.",
+      title: localization.settingsPage_personalizationSection_themeTitle,
+      description:
+          localization.settingsPage_personalizationSection_themeDescription,
       trailing: DropdownMenu(
         menuStyle: const MenuStyle(),
         initialSelection: widget.selectedTheme,
@@ -30,10 +33,19 @@ class _ThemeSelectorState extends State<ThemeSelector> {
           iconColor: theme.colorScheme.primary,
           focusColor: theme.colorScheme.primary,
         ),
-        dropdownMenuEntries: const <DropdownMenuEntry>[
-          DropdownMenuEntry(value: ThemeMode.dark, label: "Dark"),
-          DropdownMenuEntry(value: ThemeMode.light, label: "Light"),
-          DropdownMenuEntry(value: ThemeMode.system, label: "System")
+        dropdownMenuEntries: <DropdownMenuEntry>[
+          DropdownMenuEntry(
+            value: ThemeMode.dark,
+            label: localization.settingsPage_personalizationSection_themeDark,
+          ),
+          DropdownMenuEntry(
+            value: ThemeMode.light,
+            label: localization.settingsPage_personalizationSection_themeLight,
+          ),
+          DropdownMenuEntry(
+            value: ThemeMode.system,
+            label: localization.settingsPage_personalizationSection_themeSystem,
+          )
         ],
         onSelected: (value) => widget.selectTheme(value),
       ),
